cff23f3f60a1c11d7f0d584bd638e62b487b90d2
Alasdair Sykes <alasdair.sykes@gmail.com>
2020-09-25 19:37:02 +0100

    Remembered to source functions script in setup-and-run.R...

--- setup-and-run.R
+++ setup-and-run.R
@@ -5,6 +5,9 @@ library(dplyr)
 # read in data
 Dat_nest <- read_rds(project_data(path = "project-data/model-data-input-small-sample-wheat-manure-data.rds"))
 
+# source functions!
+source("ipcc-c-model-function.R")
+
 # sample down for ease of use
 set.seed(2605)
 Dat_nest <- Dat_nest %>% sample_n(50)
