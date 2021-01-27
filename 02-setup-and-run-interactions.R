87a3364effd824d8aa105de35c37b5d97e8c906c
Alasdair Sykes <alasdair.sykes@gmail.com>
2021-01-27 21:43:46 +0000

    New setup-and-run for combined measures.

---
+++ 02-setup-and-run-interactions.R
@@ -0,0 +1,51 @@
+library(tidyverse)
+library(soilc.ipcc)
+
+# read in data
+Dat_nest <- read_rds(project_data(path = "project-data/model-data-input-small-sample-wheat-manure-tillage-cc-data.rds"))
+
+# sample down for ease of use
+set.seed(2605)
+Dat_nest <- Dat_nest %>% sample_n(100) 
+
+# read in scenario setup csv
+scenarios <- read_csv("combined-measure-args.csv")
+
+# convert to arg list
+scenarios <- scenarios %>%
+  mutate(scenario_ref = paste(measure_x, measure_y, sep = "_"),
+         args = pmap(list(till_rotation, frac_remove_new, cc_type, comp_yield_tha), list),
+         args = map(args, ~set_names(.x, c("till_rotation", "frac_remove_new", "cc_type", "comp_yield_tha"))),
+         args = map(args, ~.x %>% discard(is.na)),
+         args = map(args, ~append(list(Dat_nest = Dat_nest), .x)),
+         args = set_names(args, scenario_ref))
+
+# source scenario functions
+source("model-scenario-functions.R")
+
+# write out path
+out <- project_data(path = "project-data/model-scenarios/")
+
+# run scenarios
+scenario_list <- map(scenarios$args, ~do.call(build_scenario, .x))
+write_rds(scenario_list, paste0(out, "interaction-scenarios.rds"))
+
+# build summary and write out
+summary <- scenario_list %>% 
+  map(
+    ~.x %>%
+      unnest(model_output) %>%
+      group_by(year) %>%
+      summarise(om_input = mean(om_input),
+                c_input = mean(c_input),
+                n_input = mean(n_input),
+                lignin_input = mean(lignin_input),
+                active_y = mean(active_y),
+                slow_y = mean(slow_y),
+                passive_y = mean(passive_y),
+                total_y = mean(total_y),
+                .groups = "drop")
+  )
+
+write_rds(summary, paste0(out, "model-interactino-scenario-summary.rds"))
+write_rds(summary, "model-runs/model-interaction-scenario-summary.rds")
